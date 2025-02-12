fece8c566e3e104e155648393cf3cc762f46c4c7
Lucky <aayushyadav373@gmail.com>
2025-02-12 22:00:18 +0530

    render existing rectangle

--- apps/frontend/draw/index.ts
+++ apps/frontend/draw/index.ts
@@ -1,9 +1,23 @@
+
+
+type Shapes ={
+    type:"rectangle";
+    x:number;
+    y:number;
+    height:number;
+    width:number
+}|{
+    type:"circle";
+    centerX:number;
+    centerY:number;
+    radius:number;
+}
 export function initDraw(canvas:HTMLCanvasElement) {
     const ctx = canvas.getContext("2d")
             if(!ctx){
                 return ;
             }
-            // ctx.strokeRect(25,25,100,100);
+            let existingShapes:Shapes[] =[]
             let clicked =false;
             let startX=0;
             let startY =0;
@@ -16,18 +30,35 @@ export function initDraw(canvas:HTMLCanvasElement) {
             })
             canvas.addEventListener("mouseup",(e)=>{
                 clicked=false;
-                console.log("mouseUpX =>"+e.clientX)
-                console.log("mouseUpY =>"+e.clientY)
+                let width = e.offsetX-startX;
+                let height = e.offsetY-startY;
+                const newShape:Shapes={
+                    type:"rectangle",
+                    x:startX,
+                    y:startY,
+                    height,
+                    width
+                }
+                existingShapes.push(newShape);
             })
             canvas.addEventListener("mousemove",(e)=>{
                 if (clicked) {
                     let height = e.offsetY-startY;
                     let width =e.offsetX-startX;
-                    ctx.clearRect(0,0,canvas.width,canvas.height);
+                    // ctx.clearRect(0,0,canvas.width,canvas.height);
+                    clearCanvas(ctx,canvas,existingShapes);
                     ctx.strokeStyle="white";
                     ctx.strokeRect(startX,startY,width,height);
                     
                 }
                 
             })
+}
+function clearCanvas(ctx:CanvasRenderingContext2D,canvas:HTMLCanvasElement,shapesArray:Shapes[]) {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    shapesArray.map((shape)=>{
+        if(shape.type==="rectangle"){
+        ctx.strokeRect(shape.x,shape.y,shape.width,shape.height)
+        }
+    })
 }
