8c3b2bf901577d395358d34c104c2f5b884755cd
Lucky <aayushyadav373@gmail.com>
2025-02-15 11:58:30 +0530

    added redirecting page

--- apps/frontend/components/RoomCanvas.tsx
+++ apps/frontend/components/RoomCanvas.tsx
@@ -5,14 +5,25 @@ import { wsUrl } from "@/url";
 import { useEffect, useRef, useState } from "react";
 import { Loading } from "./Loading";
 import Canvas from "./Canvas";
+import { useRouter } from "next/navigation";
+import RedirectPage from "./Redirecting";
 
 
 export default function RoomCanvas({roomId}:{roomId:string}) {
      const [socket,setSocket] = useState<WebSocket|null>(null);
      const [token, setToken] = useState<string | null>(null);
      const [isLoading,setIsLoading] = useState(true);
+     const [isRedirecting,setIsRedirecting] = useState(false);
+     const router = useRouter();
      useEffect(()=>{
       const storedToken = localStorage.getItem("token");
+      if (!storedToken) {
+        // setTimeout(() => {
+        //   router.push("/signin"); 
+        // }, 2000);
+        setIsRedirecting(true);
+        return 
+      }
     setToken(storedToken);
     setIsLoading(false);
      },[])
@@ -27,6 +38,13 @@ export default function RoomCanvas({roomId}:{roomId:string}) {
             }))
           }
       },[token]);
+      if (isRedirecting) {
+        return(
+          <div>
+            <RedirectPage message={"Unauthorized: Please log in to access this page"} destination={"/signin"} />
+          </div>
+        )
+      }
 
       if (!socket||isLoading) {
         return(
